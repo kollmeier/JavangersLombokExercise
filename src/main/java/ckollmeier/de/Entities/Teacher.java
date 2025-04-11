11d17c7e1ff1da6906d72ebdb0a0b5e11b0e8fe0
Carsten Kollmeier <carsten@ckollmeier.de>
2025-04-11 08:33:02 +0200

    Add Builder and With annotations to Teacher class
    Introduced Lombok's @Builder and @With annotations to the Teacher class, enabling easier object creation and immutability updates. This improves code readability and reduces boilerplate associated with manual implementations.

--- src/main/java/ckollmeier/de/Entities/Teacher.java
+++ src/main/java/ckollmeier/de/Entities/Teacher.java
@@ -1,11 +1,16 @@
 package ckollmeier.de.Entities;
 
+import lombok.Builder;
+import lombok.With;
+
 /**
  * Diese Klasse repräsentiert einen Lehrer mit seinen Basisinformationen.
  * @param id Eindeutige Kennung des Lehrers.
  * @param name Vollständiger Name des Lehrers.
  * @param address Wohnadresse des Lehrers.
  */
+@Builder
+@With
 public record Teacher(
         String id,
         String name,
